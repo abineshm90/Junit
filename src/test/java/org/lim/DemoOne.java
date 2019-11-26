d71493728af332b7a753f9310e0f9d94fb4a1970
Kakumanu14 <bhavanakakumanu22@gmail.com>
2019-11-26 21:54:39 +0530

    junit

---
+++ src/test/java/org/lim/DemoOne.java
@@ -0,0 +1,74 @@
+package org.lim;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import junit.framework.Assert;
+
+public class DemoOne {
+	static WebDriver driver;
+@AfterClass
+public static void close() {
+	driver.quit();
+}
+	
+@BeforeClass
+public static  void launchBrowser() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Greens-12\\eclipse-workspace\\Kiruba\\AbiMaven\\driver\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	 driver.get("https://www.facebook.com/");
+	 String u = driver.getCurrentUrl();
+	 Assert.assertTrue(u.contains("facebook"));
+	 Assert.assertEquals("https://www.facebook.com/", u);
+}
+
+@Test
+public void tc1() {
+	
+	WebElement user = driver.findElement(By.id("email"));
+	user.sendKeys("abi");
+	String s = user.getAttribute("value");
+	System.out.println(s);
+	Assert.assertEquals("abinesh",s);
+	
+	WebElement pass = driver.findElement(By.id("pass"));
+	pass.sendKeys("789");
+	String a = pass.getAttribute("value");
+	System.out.println(a);
+	Assert.assertEquals("789", a);
+}
+
+
+
+
+@Test
+public void tc2() {
+	
+	WebElement btn = driver.findElement(By.xpath("//input[@value='Log In']"));
+	btn.click();
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/test/java/org/lim/demo.java
@@ -0,0 +1,61 @@
+package org.lim;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+	static WebDriver driver;
+	@AfterClass
+	
+	public static void closeBroswer() {
+		driver.quit();
+
+	}
+	
+	@BeforeClass
+	public static void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Greens-12\\eclipse-workspace\\Kiruba\\AbiJUnit\\driver\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		 driver.get("https://www.facebook.com/");
+			
+	}
+	
+	@After
+	public void endTime() {
+		Date d = new Date();
+		System.out.println(d);
+	}
+	
+	@Before
+	public void startTime() {
+		Date d = new Date();
+		System.out.println(d);
+	}
+	
+	@Test
+	public void tc1() {
+	
+	WebElement user = driver.findElement(By.id("email"));
+	user.sendKeys("abinesh@gmail.com");
+	
+	WebElement pass = driver.findElement(By.id("pass"));
+	pass.sendKeys("123456");
+	
+	}
+	
+	@Test
+	public void tc2() {
+		WebElement btn = driver.findElement(By.xpath("//input[@value='Log In']"));
+		btn.click();
+
+	}
+}
